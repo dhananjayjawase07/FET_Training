608164d73ae4a169d5d9cab5b46abebe65000f4e
dhananjayjawase07 <97471417+dhananjayjawase07@users.noreply.github.com>
2022-02-03 23:53:25 +0530

    Boottstrap workshop  added

---
+++ Bootstrap_Workshop/js/home.js
@@ -0,0 +1,8 @@
+function search_function(){
+    var x= document.getElementById("search").value;
+    console.log(x);
+    if(x === "beetroot"){
+        window.location='index.html';
+    }
+    
+}
